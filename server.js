9122b8bc260d098dda28c392c8507bc4a42dc584
John-Mike Marquardt <jmmarquardt@gmail.com>
2016-12-17 23:05:26 -0500

    initial react framework

--- server.js
+++ server.js
@@ -4,32 +4,53 @@
 var express = require('express');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+var request = require('request');
+// mongoose mpromise is deprecated, using bluebird promises
+var Promise = require('bluebird');
+mongoose.Promise = Promise;
 
 // Sets up the Express App
 // ==========================================================================
 var app = express();
 var PORT = 3010;
 
+// Use morgan for logging and body-parser
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({	extended: false }));
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() || __dirname + '/public'));
-
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
 
 // override with POST having ?_method=DELETE
 app.use(methodOverride('_method'));
 
-var exphbs = require('express-handlebars');
+// ===========================================================================
+// MONGOOSE
+// ===========================================================================
+// config mongoDB with mongoose
+var devUri = "mongodb://localhost/nytimes";
 
-app.engine('handlebars', exphbs({
-	defaultLayout: 'main'
-}));
+if (process.env.MONGODNB_URI) {
+	mongoose.connect(process.env.MONGODNB_URI);
+} else {
+	mongoose.connect(devUri);
+};
+var db = mongoose.connection;
 
-app.set('view engine', 'handlebars');
+// log mongoose errors
+db.on("error", function(error) {
+	console.log("Mongoose Error: ", error);
+});
+// log successful connection
+db.once("open", function(){
+	console.log("Mongoose connection successful.");
+});
 
+// ROUTES
+// ===========================================================================
 var routes = require('./controllers/controller.js');
-
 app.use('/', routes);
 
 // Starts the server to begin listening
